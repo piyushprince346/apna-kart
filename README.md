72771121eade8e113e055bc11f29e4924b16248d
piyushprince346 <piyushprince346@gmail.com>
2021-07-04 01:03:44 +0530

    Implemented User Authentication System

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Getting Started with Create React App
-# Author - Piyush B Prince, Ronit Raj, Modabbir Tarique, Sugyan Mishra
+# Author - Piyush B Prince, Ronit Raj, Modabbir Tarique
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import './App.css';
 import Home from './components/Home/Home.js';
 import Header from './components/Header/Header.js';
+import Login from './components/Login/Login';
+import { auth } from './database/firebase';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useEffect } from 'react'
+import { useStateValue } from './contexts/StateProvider';
+
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/">
             <Header />
             <Home />
